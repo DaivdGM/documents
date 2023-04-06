9b4c02e39341dda8acef7af1cd25766b00387dca
david <david.garcia.monterroso@externos.gloval.es>
2023-04-06 21:17:17 +0200

    Primera prueba mongo

---
+++ src/main/java/es/reposteria/documents/Customer.java
@@ -0,0 +1,33 @@
+package es.reposteria.documents;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class Customer {
+
+  @Id
+  private String id;
+
+  private String firstName;
+  private String lastName;
+
+  public Customer() {}
+
+  public Customer(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+      "Customer[id=%s, firstName='%s', lastName='%s']",
+      id, firstName, lastName);
+  }
+
+}
